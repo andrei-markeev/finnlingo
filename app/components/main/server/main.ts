038ad1b4e575afba38e5675dab09d1e3cc908b75
Andrei Markeev <andrei-markeev@users.noreply.github.com>
2017-08-05 02:22:37 +0300

    Added publishing more user data to client

--- app/components/main/server/main.ts
+++ app/components/main/server/main.ts
@@ -11,5 +11,9 @@ Meteor.startup(() => {
         }
     );
 
+    Meteor.publish('userData', function() {
+        return Meteor.users.find({ _id: this.userId }, { fields: { study: 1 } });
+    });
+
 });
 
