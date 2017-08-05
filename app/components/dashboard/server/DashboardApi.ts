e62e46c139d49765408030f1feb0825c50b13dae
Andrei Markeev <andrei-markeev@users.noreply.github.com>
2017-08-06 01:53:36 +0300

    Fix for today leaders

--- app/components/dashboard/server/DashboardApi.ts
+++ app/components/dashboard/server/DashboardApi.ts
@@ -5,7 +5,7 @@ class DashboardApi {
         var today = new Date();
         today.setHours(0, 0, 0);
         var todayLeaders = Meteor.users.find(
-            { "study.lastDateXP": { $gte: today.getTime() } }, 
+            { "study.lastDateStudied": { $gte: today.getTime() } }, 
             { sort: { "study.lastDateXP": -1 }, limit: 10, fields: { "profile.name": 1, "study.lastDateXP": 1, "services.facebook.id": 1 } })
             .fetch()
             .map(tl => ({ 
