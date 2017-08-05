40d8d1f2486056a8d00f94b6dd60a2404643b7ef
Andrei Markeev <andrei-markeev@users.noreply.github.com>
2017-08-06 00:39:32 +0300

    Leaderboard

--- app/components/dashboard/client/DashboardComponent.ts
+++ app/components/dashboard/client/DashboardComponent.ts
@@ -7,10 +7,14 @@ class DashboardComponent {
     course: Course = null;
     loggingIn: boolean = true;
     user: User = null;
+    todayLeaders = [];
+    allTimeLeaders = [];
 
     created() {
         DashboardApi.getDashboardPageData((err, res) => {
             this.course = res.course;
+            this.todayLeaders = res.todayLeaders;
+            this.allTimeLeaders = res.allTimeLeaders;
             Tracker.autorun(() => {
                 this.loggingIn = Meteor.loggingIn();
                 this.user = Meteor.user();
--- app/components/dashboard/server/DashboardApi.ts
+++ app/components/dashboard/server/DashboardApi.ts
@@ -2,8 +2,30 @@ class DashboardApi {
     @Decorators.method
     static getDashboardPageData(callback?) {
         var user = ACL.getUserOrThrow(this);
+        var today = new Date();
+        today.setHours(0, 0, 0);
+        var todayLeaders = Meteor.users.find(
+            { "study.lastDateXP": { $gte: today.getTime() } }, 
+            { sort: { "study.lastDateXP": -1 }, limit: 10, fields: { "profile.name": 1, "study.lastDateXP": 1, "services.facebook.id": 1 } })
+            .fetch()
+            .map(tl => ({ 
+                name: tl.profile.name,
+                xp: tl.study.lastDateXP,
+                avatarUrl: "http://graph.facebook.com/" + user.services.facebook.id + "/picture"
+            }));
+        var allTimeLeaders = Meteor.users.find(
+            { }, 
+            { sort: { "study.xp": -1 }, limit: 10, fields: { "profile.name": 1, "study.xp": 1, "services.facebook.id": 1 } })
+            .fetch()
+            .map(tl => ({ 
+                name: tl.profile.name,
+                xp: tl.study.xp,
+                avatarUrl: "http://graph.facebook.com/" + user.services.facebook.id + "/picture"
+            }));
         return {
-            course: Courses.findOne(user.selectedCourseId)
+            course: Courses.findOne(user.selectedCourseId),
+            todayLeaders: todayLeaders,
+            allTimeLeaders: allTimeLeaders
         };
     }
 }
